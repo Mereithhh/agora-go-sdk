0f73896d02838c008b2b12652f2fdc294f673f85
mereith <wanglu@mereith.com>
2024-07-03 06:24:26 +0000

    fix: script

--- ld.sh
+++ ld.sh
@@ -10,11 +10,11 @@ fi
 # Check the operating system
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   # Linux
-  $SUDO cp -r agoraserver/agora_sdk/*.so /usr/lib/
+  $SUDO cp -r agoraservice/agora_sdk/*.so /usr/lib/
   $SUDO ldconfig
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS
-  $SUDO cp -r agoraserver/agora_sdk_mac/*.dylib /usr/local/lib/
+  $SUDO cp -r agoraservice/agora_sdk_mac/*.dylib /usr/local/lib/
   $SUDO update_dyld_shared_cache
 else
   echo "Unsupported operating system"
